40f0dbaa2ae74b6f3ca98919206fac20093a59c1
mfiejtek <fiejtekmichal@gmail.com>
2025-12-22 20:27:48 +0100

    dodanie komentarzy

--- Projekt/implementation/Node.py
+++ Projekt/implementation/Node.py
@@ -1,8 +1,21 @@
+"""Moduł definiujący węzeł drzewa czerwono-czarnego.
+
+Zawiera stałe kolorów `RED` / `BLACK` oraz klasę `Node` przechowującą
+pola potrzebne do budowy drzewa (wartość, rodzic, lewe/prawe dziecko, kolor).
+"""
+
 # Stałe dla kolorów
 RED = 1
 BLACK = 0
 
 class Node:
+    """Reprezentuje pojedynczy węzeł drzewa.
+
+    Atrybuty:
+        val: przechowywana wartość klucza
+        parent, left, right: wskaźniki na inne węzły
+        color: kolor węzła (`RED` lub `BLACK`)
+    """
     def __init__(self, val):
         self.val = val
         self.parent = None
--- Projekt/implementation/RedBlackTree.py
+++ Projekt/implementation/RedBlackTree.py
@@ -1,9 +1,19 @@
+"""Implementacja drzewa czerwono-czarnego (Red-Black Tree).
+
+Klasa dostarcza metody do wstawiania i usuwania elementów oraz pomocnicze
+operacje potrzebne do utrzymania własności drzewa.
+"""
+
 from implementation.Node import BLACK, RED, Node
 
 class RedBlackTree:
+    """Struktura drzewa czerwono-czarnego z operacjami modyfikującymi.
+
+    Atrybuty:
+        TNULL: sentinel reprezentujący puste potomki (czarny)
+        root: korzeń drzewa
+    """
     def __init__(self):
-        # TNULL to nasz "wartownik" reprezentujący liście (NIL)
-        # Dzięki temu unikamy błędów przy odwoływaniu się do atrybutów None
         self.TNULL = Node(0)
         self.TNULL.color = BLACK
         self.TNULL.left = None
@@ -11,6 +21,10 @@ class RedBlackTree:
         self.root = self.TNULL
 
     def left_rotate(self, x):
+        """Wykonuje lewą rotację wokół węzła x.
+
+        Używane przy przywracaniu równowagi drzewa.
+        """
         y = x.right
         x.right = y.left
         if y.left != self.TNULL:
@@ -28,6 +42,10 @@ class RedBlackTree:
         x.parent = y
 
     def right_rotate(self, x):
+        """Wykonuje prawą rotację wokół węzła x.
+
+        Używane przy przywracaniu równowagi drzewa.
+        """
         y = x.left
         x.left = y.right
         if y.right != self.TNULL:
@@ -46,10 +64,15 @@ class RedBlackTree:
 
 
     def fix_insert(self, k):
+        """Naprawia własności drzewa po wstawieniu węzła k.
+
+        Przywraca właściwości drzewa czerwono-czarnego poprzez odpowiednie
+        rotacje i zmiany kolorów węzłów.
+        """
         while k.parent.color == RED:
             if k.parent == k.parent.parent.right:
                 u = k.parent.parent.left
-                
+
                 if u.color == RED:
                     u.color = BLACK
                     k.parent.color = BLACK
@@ -59,7 +82,7 @@ class RedBlackTree:
                     if k == k.parent.left:
                         k = k.parent
                         self.right_rotate(k)
-                    
+
                     k.parent.color = BLACK
                     k.parent.parent.color = RED
                     self.left_rotate(k.parent.parent)
@@ -75,17 +98,23 @@ class RedBlackTree:
                     if k == k.parent.right:
                         k = k.parent
                         self.left_rotate(k)
-                    
+
                     k.parent.color = BLACK
                     k.parent.parent.color = RED
                     self.right_rotate(k.parent.parent)
-            
+
             if k == self.root:
                 break
-        
+
         self.root.color = BLACK
-    
+
     def insert(self, key):
+        """Wstawia wartość `key` do drzewa.
+
+        Tworzy nowy węzeł i wstawia go zgodnie z własnością BST,
+        a następnie wywołuje `fix_insert` w celu przywrócenia
+        własności czerwono-czarnych.
+        """
         node = Node(key)
         node.parent = None
         node.val = key
@@ -123,6 +152,10 @@ class RedBlackTree:
     # --- METODY POMOCNICZE DO USUWANIA ---
 
     def rb_transplant(self, u, v):
+        """Zastępuje poddrzewo węzła `u` poddrzewem `v`.
+
+        Używane wewnętrznie przy usuwaniu węzłów.
+        """
         if u.parent is None:
             self.root = v
         elif u == u.parent.left:
@@ -132,23 +165,26 @@ class RedBlackTree:
         v.parent = u.parent
 
     def minimum(self, node):
+        """Zwraca węzeł o najmniejszej wartości w poddrzewie `node`."""
         while node.left != self.TNULL:
             node = node.left
         return node
 
-    # --- NAPRAWA PO USUNIĘCIU  ---
-
     def fix_delete(self, x):
+        """Naprawia własności drzewa po usunięciu węzła wskazywanego przez x.
+
+        Operacja przywraca równowagę drzewa poprzez rotacje i zmiany kolorów.
+        """
         while x != self.root and x.color == BLACK:
             if x == x.parent.left:
-                w = x.parent.right 
-                
+                w = x.parent.right
+
                 if w.color == RED:
                     w.color = BLACK
                     x.parent.color = RED
                     self.left_rotate(x.parent)
                     w = x.parent.right
-                
+
                 if w.left.color == BLACK and w.right.color == BLACK:
                     w.color = RED
                     x = x.parent
@@ -158,7 +194,7 @@ class RedBlackTree:
                         w.color = RED
                         self.right_rotate(w)
                         w = x.parent.right
-                    
+
                     w.color = x.parent.color
                     x.parent.color = BLACK
                     w.right.color = BLACK
@@ -166,13 +202,13 @@ class RedBlackTree:
                     x = self.root
             else:
                 w = x.parent.left
-                
+
                 if w.color == RED:
                     w.color = BLACK
                     x.parent.color = RED
                     self.right_rotate(x.parent)
                     w = x.parent.left
-                
+
                 if w.right.color == BLACK and w.left.color == BLACK:
                     w.color = RED
                     x = x.parent
@@ -182,21 +218,27 @@ class RedBlackTree:
                         w.color = RED
                         self.left_rotate(w)
                         w = x.parent.left
-                    
+
                     w.color = x.parent.color
                     x.parent.color = BLACK
                     w.left.color = BLACK
                     self.right_rotate(x.parent)
                     x = self.root
-        
+
         x.color = BLACK
 
     # --- GŁÓWNA METODA USUWANIA ---
 
     def delete_node(self, val):
+        """Usuń węzeł o wartości `val` z drzewa."""
         return self._delete_node_helper(self.root, val)
 
     def _delete_node_helper(self, node, key):
+        """Pomocnicza implementacja usuwania.
+
+        Znajduje węzeł o danym kluczu, usuwa go zachowując własność BST,
+        a następnie wywołuje `fix_delete` jeśli było to konieczne.
+        """
         z = self.TNULL
         while node != self.TNULL:
             if node.val == key:
